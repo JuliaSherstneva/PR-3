70081b26115a93869c31b728fe6bf2c4eca087c1
исп-22 <-22@LAB30-04>
2024-10-14 14:26:24 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//4.Даны два произвольных числа a, b. Если оба значения принадлежат отрезку [c,d],
+//то вычислить R=sin(a)sin(b), в противном случае вывести сообщение «Нет решения»
+
+
+double a, b, c, d, R;
+        Console.WriteLine("Введите значение");
+c = Convert.ToInt32(Console.ReadLine());
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+d = Convert.ToInt32(Console.ReadLine());
+
+if (c >= a && a <= d && c >= b && b <= d) 
+        {
+    R = (double)(Math.Sin(a) * Math.Sin(b));
+    Console.WriteLine("Ответ R=" + R);
+}
+else
+{
+    Console.WriteLine("Нет решения");
+}
+Console.ReadKey();
